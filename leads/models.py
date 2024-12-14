1011517bc7dc9d8a54ef63303c3067d47496b42e
jijovaliyaveettil <v.francisjijo@gmail.com>
2024-12-14 06:53:37 +0200

    added leads model

---
+++ leads/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Lead(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
