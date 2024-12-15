eef2f28452ce6a66a95efbb2a12f135f0be31d6a
jijovaliyaveettil <v.francisjijo@gmail.com>
2024-12-15 14:34:52 +0200

    added user, agent annd leads

---
+++ leads/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    age = models.IntegerField(default=0)
+    phone_number = models.CharField(max_length=20, blank=True)
+    agent = models.ForeignKey('Agent', on_delete=models.SET_NULL, null=True)
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
